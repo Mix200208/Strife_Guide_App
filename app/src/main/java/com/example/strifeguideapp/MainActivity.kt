20254f176f4ea0519680d04f3a0644e0fac5f0d5
Ace <58069279+Mix200208@users.noreply.github.com>
2021-06-20 18:14:35 +0300

    Fragments

--- app/src/main/java/com/example/strifeguideapp/MainActivity.kt
+++ app/src/main/java/com/example/strifeguideapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.strifeguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       val navController = findNavController(R.id.fragment)
+
+       val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+       bottomNavigationView.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/example/strifeguideapp/MenyActivity.kt
+++ app/src/main/java/com/example/strifeguideapp/MenyActivity.kt
@@ -2,10 +2,15 @@ package com.example.strifeguideapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MenyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meny)
+
+
     }
 }
