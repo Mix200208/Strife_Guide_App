dd074ea356340b9a7dbfee18c5757d68c73bc5e0
Yaroslav <you@example.com>
2021-07-01 16:07:17 +0300

    create ViewDetailHero

---
+++ app/src/main/java/com/example/strifeguideapp/HeroDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.strifeguideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HeroDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hero_detail)
+    }
+}
